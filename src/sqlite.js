3f2529e674f643e41c4dbec0bea61145d71a6569
lanmower <lanmower@users.noreply.github.com>
2025-11-26 16:56:22 +0200

    Update sqlite adaptor

--- src/sqlite.js
+++ src/sqlite.js
@@ -1,6 +1,6 @@
 import initSqlJs from 'sql.js';
-import { StorageAdapter } from 'tasker-adaptor';
-import { Serializer, CRUDPatterns, RECORD_TYPES } from 'tasker-storage-utils';
+import { StorageAdapter } from 'sequential-adaptor';
+import { Serializer, CRUDPatterns, RECORD_TYPES } from 'sequential-storage-utils';
 import logger from 'tasker-logging';
 import fs from 'fs';
 import path from 'path';
